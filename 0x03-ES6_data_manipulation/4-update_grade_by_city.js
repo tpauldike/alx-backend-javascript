57bc4f67173a30d374da33f6ffd2375d5a2e08e8
tpauldike <topman4loveworld@gmail.com>
2023-08-01 19:09:35 +0000

    added .eslint configurations

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-prototype-builtins */
 export default function updateStudentGradeByCity(list, city, newGrade) {
   return list
     .filter((obj) => obj.location === city)
     .map((student) => {
-    //   console.log(newGrade);
+
       newGrade.map((studentGrade) => {
         if (studentGrade.studentId === student.id) {
           // eslint-disable-next-line no-param-reassign
